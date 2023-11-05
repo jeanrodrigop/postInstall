c6a4620ec1c3d4af11853e7f20a20ba0dc4c23ec
Jean Rodrigo <jeanrodrigo_p@outlook.com>
2023-11-04 23:36:49 -0300

    New fedora post install

---
+++ scripts/fedora.sh
@@ -0,0 +1,171 @@
+#!/usr/bin/env bash
+## ---------------------------------------------------------------------- ##
+##  postinstall.sh - Personal script for automate linux post installation 
+##  Autor: Jean Rodrigo
+## ---------------------------------------------------------------------- ##
+##  HOW TO USE?
+##  $ ./postinstall.sh
+## ----------------------------- VARIABLES ------------------------------ ##
+set -e
+
+BASHRC="$HOME/.bashrc"
+
+#COLORS
+RED='\e[1;91m'
+GREEN='\e[1;92m'
+NO_COLOR='\e[0m'
+
+#FUNCTIONS
+# -------------------------------------------------------------------------------- #
+# --------------------------------CHANGE HOSTNAME--------------------------------- #
+# Set new hostname
+set_hostname(){
+  read -p "Enter the new hostname: " NEW_HOSTNAME
+  if [[ $NEW_HOSTNAME =~ ^[a-zA-Z0-9-]+$ ]]; then
+      sudo hostnamectl set-hostname "$NEW_HOSTNAME"
+      echo "$NEW_HOSTNAME" | sudo tee /etc/hostname > /dev/null
+      echo -e "Hostname set to $NEW_HOSTNAME"
+  else
+      echo "Invalid hostname. Use only letters, numbers and hyphens."
+  fi
+}
+
+# -------------------------------------------------------------------------------- #
+# ------------------------------TEST AND REQUIREMENTS----------------------------- #
+# Internet test
+internet_test(){
+  if ! ping -c 1 1.1.1.1 -q &> /dev/null; then
+    echo -e "${RED}[ERROR] - Your computer does not have an Internet connection. Check the network.${NO_COLOR}"
+    exit 1
+  else
+    echo -e "${GREEN}[INFO] - Internet connection ok!${NO_COLOR}"
+  fi
+}
+
+# Update repositories and upgrade system
+repo_update(){
+  sudo dnf update -y && sudo dnf upgrade -y --refresh
+}
+
+# Fastest mirror config
+fast_mirror(){
+  sudo echo "max_parallel_downloads=10"  >> /etc/dnf/dnf.conf
+  sudo echo "fastestmirror=True"  >> /etc/dnf/dnf.conf
+  sudo echo "deltarpm=True"  >> /etc/dnf/dnf.conf
+}
+
+fusion_repo(){
+  sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+  sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+}
+
+# -------------------------------------------------------------------------- #
+## SOFTWARES TO INSTALL
+PROGRAMS_TO_INSTALL=(
+  snapd
+  flatpak
+  gparted
+  git
+  htop
+  neofetch
+  vim
+  wget
+  curl
+  vlc
+  unzip
+  p7zip
+  p7zip-plugins
+  unrar
+  fedora-workstation-repositories
+)
+
+# Installing programs from repositories
+install_dnf(){
+  echo -e "${GREEN}[INFO] - Installing programs from repositories${NO_COLOR}"
+
+  for program_name in ${PROGRAMS_TO_INSTALL[@]}; do
+    sudo dnf install "$program_name" -y
+  done
+}
+
+chrome_install(){
+  sudo dnf config-manager --set-enabled google-chrome
+  sudo dnf install google-chrome-stable -y
+}
+
+remove_firefox(){
+  sudo dnf remove -y firefox
+}
+
+snap_symbolic(){
+  sudo ln -s /var/lib/snapd/snap /snap
+}
+
+## Set flathub repository
+flat_repo(){
+  flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+}
+
+## Installing Flatpak packages ##
+install_flatpaks(){
+  echo -e "${GREEN}[INFO] - Installing flatpak packages${NO_COLOR}"
+  flatpak install flathub com.visualstudio.code -y
+}
+
+multimedia_plugins(){
+  sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel
+  sudo dnf install lame\* --exclude=lame-devel
+  sudo dnf group upgrade --with-optional Multimedia
+}
+
+firmware_update(){
+  sudo fwupdmgr get-devices -y
+  sudo fwupdmgr refresh -y --force 
+  sudo fwupdmgr get-updates -y 
+  sudo fwupdmgr update -y
+
+}
+
+# -------------------------------------------------------------------------- #
+# ------------------------------ POST-INSTALL ------------------------------ #
+## Finalizing, updating and cleaning ##
+system_clean(){
+  repo_update -y
+  flatpak update -y
+  sudo dnf clean all
+}
+
+# -------------------------------------------------------------------------- #
+# ----------------------------- EXTRA CONFIGS ------------------------------ #
+extra_config(){
+  #Creating aliases in ~/.bashrc file
+  sudo echo "alias updf='sudo dnf update -y && sudo dnf upgrade -y --refresh'" >> ../.bashrc
+
+  #Changing current swappiness value
+  sudo echo "vm.swappiness=10"  >> /etc/sysctl.conf
+
+  source $BASHRC
+}
+
+# -------------------------------------------------------------------------------- #
+# ------------------------------- RUNNING ---------------------------------------- #
+
+set_hostname
+internet_test
+fast_mirror
+fusion_repo
+repo_update
+install_dnf
+chrome_install
+remove_firefox
+snap_symbolic
+flat_repo
+multimedia_plugins
+install_flatpaks
+firmware_update
+repo_update
+system_clean
+extra_config
+
+## finalização
+  echo -e "${GREEN}[INFO] - Script completed, reboot the system! :)${NO_COLOR}"
